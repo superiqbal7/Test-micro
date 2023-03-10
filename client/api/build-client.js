783cb507efe9763e07f0a9e021d18fc31ef2bf67
Rahim Iqbal <superiqu@gmail.com>
2023-03-11 05:48:34 +0600

    fix k8 issues

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: "http://www.YOUR_DOMAIN_NAME",
+      baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
